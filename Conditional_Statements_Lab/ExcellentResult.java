84629e0fd5bde582e53abf585b3ee8025269e39c
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-01-22 15:40:45 +0200

    Add files via upload

---
+++ Conditional_Statements_Lab/ExcellentResult.java
@@ -0,0 +1,18 @@
+package ConditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class ExcellentResult {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int grade = Integer.parseInt(scanner.nextLine());
+
+        if(grade >= 5){
+            System.out.println("Excellent!");
+        } else {
+            System.out.println("The grade is smaller than 5!");
+        }
+    }
+}
---
+++ Conditional_Statements_Lab/ToyShop.java
@@ -0,0 +1,44 @@
+package ConditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class ToyShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double tripPrice = Double.parseDouble(scanner.next());
+        int puzzles = Integer.parseInt(scanner.next());
+        int dolls = Integer.parseInt(scanner.next());
+        int bears = Integer.parseInt(scanner.next());
+        int minions = Integer.parseInt(scanner.next());
+        int trucks = Integer.parseInt(scanner.next());
+
+        double toysPrice =  puzzles * 2.60 + dolls * 3.00 + bears * 4.10 + minions * 8.20 + trucks * 2.00;
+        int toys = puzzles + dolls + bears + minions + trucks;
+
+        if(toys >= 50){
+            double priceAfterDiscount = toysPrice - toysPrice * 0.25;
+            double rent = priceAfterDiscount - priceAfterDiscount * 0.10;
+            if(rent >= tripPrice){
+                double result = rent - tripPrice;
+                System.out.printf("Yes! %.2f lv left.", result);
+            }
+            else if (rent < tripPrice){
+                double result = tripPrice - rent;
+                System.out.printf("Not enough money! %.2f lv needed.", result);
+            }
+        }
+        else if (toys < 49){
+            double rent = toysPrice - toysPrice * 0.10;
+            if(rent >= tripPrice){
+                double result = rent - tripPrice;
+                System.out.printf("Yes! %.2f lv left.", result);
+            }
+            else if(rent < tripPrice){
+                double result = tripPrice - rent;
+                System.out.printf("Not enough money! %.2f lv needed.", result);
+            }
+        }
+    }
+}
+
