e0db51f9b1a1da5725b2bf6f387b79dbdc61859b
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-01-22 15:44:30 +0200

    Add files via upload

---
+++ Conditional_Statements_Advanced_Lab/SkiTrip.java
@@ -0,0 +1,55 @@
+package ConditionalStatementsAdvancedLab;
+
+import java.util.Scanner;
+
+public class SkiTrip {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int days = Integer.parseInt(scanner.nextLine());
+        String roomType = scanner.nextLine();
+        String feedback = scanner.nextLine();
+
+        int nightsCount = days - 1;
+
+        double price = 0.0;
+
+        switch (roomType) {
+            case "room for one person":
+                price = 18;
+                break;
+            case "apartment":
+                price = 25;
+                if (nightsCount < 10) {
+                    price = price * 0.7;
+                }
+                else if (nightsCount <= 15) {
+                    price = price * 0.65;
+                }
+                else if (nightsCount > 15) {
+                    price = price * 0.5;
+                }
+                break;
+            case "president apartment":
+                price = 35;
+                if (nightsCount < 10) {
+                    price = price * 0.9;
+                }
+                else if (nightsCount <= 15) {
+                    price = price * 0.85;
+                }
+                else if (nightsCount > 15) {
+                    price = price * 0.8;
+                }
+                break;
+        }
+        double totalPrice = price * nightsCount;
+        if (feedback.equals("positive")) {
+            totalPrice = totalPrice * 1.25;
+        }
+        else if (feedback.equals("negative")) {
+            totalPrice = totalPrice * 0.9;
+        }
+        System.out.printf("%.2f", totalPrice);
+    }
+}
