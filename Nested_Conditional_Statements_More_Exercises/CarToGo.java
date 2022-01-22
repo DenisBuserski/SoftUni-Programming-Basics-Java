5bf5e070fd9d3a3e50365bbb5adfa7afc883695a
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-01-22 15:54:47 +0200

    Add files via upload

---
+++ Nested_Conditional_Statements_More_Exercises/CarToGo.java
@@ -0,0 +1,51 @@
+package NestedConditionalStatementsMoreExercises;
+
+import java.util.Scanner;
+
+public class CarToGo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double budget = Double.parseDouble(scanner.nextLine());
+        String season = scanner.nextLine();
+
+        String classCar = "";
+        String typeCar = "";
+        double price = 0.0;
+
+        if (budget <= 100) {
+            classCar = "Economy class";
+            if (season.equals("Summer")) {
+                typeCar = "Cabrio";
+                price = budget * 0.35;
+            }
+            else if (season.equals("Winter")) {
+                typeCar = "Jeep";
+                price = budget * 0.65;
+            }
+        }
+        else if (budget > 100 && budget <= 500) {
+            classCar = "Compact class";
+            if (season.equals("Summer")) {
+                typeCar = "Cabrio";
+                price = budget * 0.45;
+            }
+            else if (season.equals("Winter")) {
+                typeCar = "Jeep";
+                price = budget * 0.8;
+            }
+        }
+        else if (budget > 500) {
+            classCar = "Luxury class";
+            typeCar = "Jeep";
+            price = budget * 0.9;
+        }
+
+        System.out.println(classCar);
+        System.out.printf("%s - %.2f", typeCar, price);
+
+
+
+
+    }
+}
---
+++ Nested_Conditional_Statements_More_Exercises/MultiplyBy2.java
@@ -0,0 +1,24 @@
+package NestedConditionalStatementsMoreExercises;
+
+import java.util.Scanner;
+
+public class MultiplyBy2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i >= 0; i++) {
+            double number = Double.parseDouble(scanner.nextLine());
+
+            if (number >= 0) {
+                number = number * 2;
+                System.out.printf("Result: %.2f%n", number);
+
+            }
+            else {
+                System.out.println("Negative number!");
+                break;
+            }
+        }
+
+    }
+}
