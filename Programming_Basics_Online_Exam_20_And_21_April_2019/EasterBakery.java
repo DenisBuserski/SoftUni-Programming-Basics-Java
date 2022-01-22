14bf271e6ca83ff32f315fcebb1c62c8dc52468d
Denis Buserski <81383938+DenisBuserski@users.noreply.github.com>
2022-01-22 16:05:58 +0200

    Add files via upload

---
+++ Programming_Basics_Online_Exam_20_And_21_April_2019/EasterBakery.java
@@ -0,0 +1,30 @@
+package ProgrammingBasicsOnlineExam20And21April2019;
+
+import java.util.Scanner;
+
+public class EasterBakery {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double flourPrice = Double.parseDouble(scanner.nextLine());
+        double flourKg = Double.parseDouble(scanner.nextLine());
+        double sugarKg = Double.parseDouble(scanner.nextLine());
+        int eggsNum = Integer.parseInt(scanner.nextLine());
+        int yeast = Integer.parseInt(scanner.nextLine());
+
+        double sugarPrice = flourPrice * 0.75;
+        double eggsPrice = flourPrice * 1.1;
+        double yeastPrice = sugarPrice * 0.2;
+
+        double flourSum = flourPrice * flourKg;
+        double sugarSum = sugarPrice * sugarKg;
+        double eggsSum = eggsPrice * eggsNum;
+        double yeastSum = yeastPrice * yeast;
+
+        double finalResult = flourSum + sugarSum + eggsSum + yeastSum;
+        System.out.printf("%.2f", finalResult);
+
+
+
+    }
+}
