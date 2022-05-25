e6117f7b0af49c4ff53ea5d4a1337598cb1f6db1
Denis Buserski <denis.buserski@gmail.com>
2022-05-25 08:37:45 +0300

    Update and rename SummerOutfit.java to Summer_Outfit_02.java

--- Conditional_Statements_Advanced_Exercise/Summer_Outfit_02.java
+++ Conditional_Statements_Advanced_Exercise/Summer_Outfit_02.java
@@ -1,8 +1,6 @@
-package ConditionalStatementsAdvancedExercise;
-
 import java.util.Scanner;
 
-public class SummerOutfit {
+public class Summer_Outfit_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,33 +11,27 @@ public class SummerOutfit {
             case "Morning":
                 if (degrees >= 10 & degrees <= 18){
                     System.out.printf("It's %d degrees, get your Sweatshirt and Sneakers.", degrees);
-                }
-                else if (degrees > 18 && degrees <= 24) {
+                } else if (degrees > 18 && degrees <= 24) {
                     System.out.printf("It's %d degrees, get your Shirt and Moccasins.", degrees);
-                }
-                else if (degrees >= 25) {
+                } else if (degrees >= 25) {
                     System.out.printf("It's %d degrees, get your T-Shirt and Sandals.", degrees);
                 }
                 break;
             case "Afternoon":
                 if (degrees >= 10 & degrees <= 18){
                     System.out.printf("It's %d degrees, get your Shirt and Moccasins.", degrees);
-                }
-                else if (degrees > 18 && degrees <= 24) {
+                } else if (degrees > 18 && degrees <= 24) {
                     System.out.printf("It's %d degrees, get your T-Shirt and Sandals.", degrees);
-                }
-                else if (degrees >= 25) {
+                } else if (degrees >= 25) {
                     System.out.printf("It's %d degrees, get your Swim Suit and Barefoot.", degrees);
                 }
                 break;
             case "Evening":
                 if (degrees >= 10 & degrees <= 18){
                     System.out.printf("It's %d degrees, get your Shirt and Moccasins.", degrees);
-                }
-                else if (degrees > 18 && degrees <= 24) {
+                } else if (degrees > 18 && degrees <= 24) {
                     System.out.printf("It's %d degrees, get your Shirt and Moccasins.", degrees);
-                }
-                else if (degrees >= 25) {
+                } else if (degrees >= 25) {
                     System.out.printf("It's %d degrees, get your Shirt and Moccasins.", degrees);
                 }
                 break;
