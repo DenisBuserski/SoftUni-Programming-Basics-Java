f2a8101c40569ada0b018f9d735494e30afd8f03
Denis Buserski <denis.buserski@gmail.com>
2022-05-27 11:50:16 +0300

    Update Easter_Guests_02.java

--- Programming_Basics_Online_Exam_20_And_21_April_2019/Easter_Guests_02.java
+++ Programming_Basics_Online_Exam_20_And_21_April_2019/Easter_Guests_02.java
@@ -1,8 +1,6 @@
-package ProgrammingBasicsOnlineExam20And21April2019;
-
 import java.util.Scanner;
 
-public class EasterGuests {
+public class Easter_Guests_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -14,20 +12,14 @@ public class EasterGuests {
         double easterBreadPrice = 4 * easterBreadNum;
         double eggsPrice = 0.45 * eggsNum;
         double finalPrice = easterBreadPrice + eggsPrice;
+        
         if (finalPrice <= budget) {
            System.out.printf("Lyubo bought %.0f Easter bread and %.0f eggs.%n", easterBreadNum, eggsNum);
              System.out.printf("He has %.2f lv. left.", budget - finalPrice);
-        }
-        else {
+        } else {
             System.out.printf("Lyubo doesn't have enough money.%n");
                   System.out.printf("He needs %.2f lv. more.", finalPrice - budget);
         }
-
-
-
-
-
-
 
     }
 }
