b170276cfb1529bf8743dd7becb84aa0fedf5085
Denis Buserski <denis.buserski@gmail.com>
2022-06-01 18:11:43 +0300

    Update and rename SmallShop.java to Small_Shop_05.java

--- Conditional_Statements_Advanced_Lab/Small_Shop_05.java
+++ Conditional_Statements_Advanced_Lab/Small_Shop_05.java
@@ -1,8 +1,6 @@
-package ConditionalStatementsAdvancedLab;
-
 import java.util.Scanner;
 
-public class SmallShop {
+public class Small_Shop_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,56 +10,43 @@ public class SmallShop {
 
         double price = 0.0;
 
-
         switch (city) {
             case "Sofia":
                 if (product.equals("coffee")) {
                     price = 0.50;
-                }
-                else if (product.equals("water")){
+                } else if (product.equals("water")){
                     price = 0.80;
-                }
-                else if (product.equals("beer")) {
+                } else if (product.equals("beer")) {
                     price = 1.20;
-                }
-                else if (product.equals("sweets")) {
+                } else if (product.equals("sweets")) {
                     price = 1.45;
-                }
-                else if (product.equals("peanuts")){
+                } else if (product.equals("peanuts")){
                     price = 1.60;
                 }
                 break;
             case "Plovdiv":
                 if (product.equals("coffee")) {
                     price = 0.40;
-                }
-                else if (product.equals("water")){
+                } else if (product.equals("water")){
                     price = 0.70;
-                }
-                else if (product.equals("beer")) {
+                } else if (product.equals("beer")) {
                     price = 1.15;
-                }
-                else if (product.equals("sweets")) {
+                } else if (product.equals("sweets")) {
                     price = 1.30;
-                }
-                else if (product.equals("peanuts")){
+                } else if (product.equals("peanuts")){
                     price = 1.50;
                 }
                 break;
             case "Varna":
                 if (product.equals("coffee")) {
                     price = 0.45;
-                }
-                else if (product.equals("water")){
+                } else if (product.equals("water")){
                     price = 0.70;
-                }
-                else if (product.equals("beer")) {
+                } else if (product.equals("beer")) {
                     price = 1.10;
-                }
-                else if (product.equals("sweets")) {
+                } else if (product.equals("sweets")) {
                     price = 1.35;
-                }
-                else if (product.equals("peanuts")){
+                } else if (product.equals("peanuts")){
                     price = 1.55;
                 }
                 break;
