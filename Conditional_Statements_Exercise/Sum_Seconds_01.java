89dc6f2c25cd6d5513d6503241f11f21f7702901
Denis Buserski <denis.buserski@gmail.com>
2022-06-02 12:54:19 +0300

    Update and rename SumSeconds.java to Sum_Seconds_01.java

--- Conditional_Statements_Exercise/Sum_Seconds_01.java
+++ Conditional_Statements_Exercise/Sum_Seconds_01.java
@@ -1,8 +1,6 @@
-package ConditionalStatementsExercise;
-
 import java.util.Scanner;
 
-public class SumSeconds {
+public class Sum_Seconds_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,15 +9,14 @@ public class SumSeconds {
         int timeThird = Integer.parseInt(scanner.nextLine());
 
         int totalTime = timeFirst + timeSecond + timeThird;
-
         int minutes = totalTime / 60;
         int seconds = totalTime % 60;
 
         if(seconds < 10){
             System.out.printf("%d:0%d", minutes, seconds);
-        }
-        else {
+        } else {
             System.out.printf("%d:%d", minutes, seconds);
         }
+        
     }
 }
