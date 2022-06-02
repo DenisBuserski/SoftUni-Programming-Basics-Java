1d581f05a650f2c27213ce98cfd58e08c0eccb35
Denis Buserski <denis.buserski@gmail.com>
2022-06-02 12:46:19 +0300

    Update and rename FruitShop.java to Fruit_Shop_11.java

--- Conditional_Statements_Advanced_Lab/Fruit_Shop_11.java
+++ Conditional_Statements_Advanced_Lab/Fruit_Shop_11.java
@@ -1,8 +1,6 @@
-package ConditionalStatementsAdvancedLab;
-
 import java.util.Scanner;
 
-public class FruitShop {
+public class Fruit_Shop_11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -14,83 +12,70 @@ public class FruitShop {
             case "banana":
                 if (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday")) {
                     System.out.printf("%.2f", num * 2.50);
-                }
-                else if (day.equals("Saturday") || day.equals("Sunday")) {
+                } else if (day.equals("Saturday") || day.equals("Sunday")) {
                     System.out.printf("%.2f", num * 2.70);
-                }
-                else {
+                } else {
                     System.out.println("error");
                 }
                 break;
             case "apple":
                 if (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday")) {
                     System.out.printf("%.2f", num * 1.20);
-                }
-                else if (day.equals("Saturday") || day.equals("Sunday")) {
+                } else if (day.equals("Saturday") || day.equals("Sunday")) {
                     System.out.printf("%.2f", num * 1.25);
-                }
-                else {
+                } else {
                     System.out.println("error");
                 }
                 break;
             case "orange":
                 if (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday")) {
                     System.out.printf("%.2f", num * 0.85);
-                }
-                else if (day.equals("Saturday") || day.equals("Sunday")) {
+                } else if (day.equals("Saturday") || day.equals("Sunday")) {
                     System.out.printf("%.2f", num * 0.90);
-                }
-                else {
+                } else {
                     System.out.println("error");
                 }
                 break;
             case "grapefruit":
                 if (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday")) {
                     System.out.printf("%.2f", num * 1.45);
-                }
-                else if (day.equals("Saturday") || day.equals("Sunday")) {
+                } else if (day.equals("Saturday") || day.equals("Sunday")) {
                     System.out.printf("%.2f", num * 1.60);
-                }
-                else {
+                } else {
                     System.out.println("error");
                 }
                 break;
             case "kiwi":
                 if (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday")) {
                     System.out.printf("%.2f", num * 2.70);
-                }
-                else if (day.equals("Saturday") || day.equals("Sunday")) {
+                } else if (day.equals("Saturday") || day.equals("Sunday")) {
                     System.out.printf("%.2f", num * 3.00);
-                }
-                else {
+                } else {
                     System.out.println("error");
                 }
                 break;
             case "pineapple":
                 if (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday")) {
                     System.out.printf("%.2f", num * 5.50);
-                }
-                else if (day.equals("Saturday") || day.equals("Sunday")) {
+                } else if (day.equals("Saturday") || day.equals("Sunday")) {
                     System.out.printf("%.2f", num * 5.60);
-                }
-                else {
+                } else {
                     System.out.println("error");
                 }
                 break;
             case "grapes":
                 if (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday")) {
                     System.out.printf("%.2f", num * 3.85);
-                }
-                else if (day.equals("Saturday") || day.equals("Sunday")) {
+                } else if (day.equals("Saturday") || day.equals("Sunday")) {
                     System.out.printf("%.2f", num * 4.20);
-                }
-                else {
+                } else {
                     System.out.println("error");
                 }
                 break;
             default:
                 System.out.println("error");
-                        break;
+                break;
         }
+        
     }
 }
