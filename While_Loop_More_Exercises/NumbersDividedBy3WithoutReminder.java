a1e6131ecbbbfe122fc7ed2751805393015ec128
Denis Buserski <denis.buserski@gmail.com>
2022-06-03 22:58:43 +0300

    Update NumbersDividedBy3WithoutReminder.java

--- While_Loop_More_Exercises/NumbersDividedBy3WithoutReminder.java
+++ While_Loop_More_Exercises/NumbersDividedBy3WithoutReminder.java
@@ -2,16 +2,13 @@ package WhileLoopMoreExercises;
 
 import java.util.Scanner;
 
-public class NumbersDividedBy3WithoutReminder {
+public class Numbers_Divided_By_3_Without_Reminder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
 
         for (int i = 3; i <= 99; i += 3) {
             System.out.println(i);
         }
-
-
 
     }
 }
