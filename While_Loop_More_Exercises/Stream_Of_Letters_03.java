f4d0d2eb91bf53d3f5b2de7d44339b05f0a0e1d8
Denis Buserski <denis.buserski@gmail.com>
2022-06-04 22:13:49 +0300

    Update and rename StreamOfLetters.java to Stream_Of_Letters_03.java

--- While_Loop_More_Exercises/Stream_Of_Letters_03.java
+++ While_Loop_More_Exercises/Stream_Of_Letters_03.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-public class Stream_Of_Letters {
+public class Stream_Of_Letters_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String word = "";
-
-        // булевите променливи помнят дали даден символ е сращнат вече веднъж
+        // Булевите променливи помнят дали даден символ е сращнат вече веднъж
         boolean o = false;
         boolean c = false;
         boolean n = false;
@@ -16,10 +15,9 @@ public class Stream_Of_Letters {
             char valid = input.charAt(0);
 
             if ((valid >= 'a' && valid <= 'z') || (valid >= 'A' && valid <= 'Z')) {
-
                 switch (input) {
                     case "o":
-                        // при първо среща на 'о' променливата о=false и не влиза в if след което о=true и при следваща среща о=true и вече влиза в if и се преибавя към текущата дума
+                        // При първо среща на 'о' променливата о=false и не влиза в if след което о=true и при следваща среща о=true и вече влиза в if и се преибавя към текущата дума
                         if (o){
                             word += input;
                         }
@@ -41,7 +39,6 @@ public class Stream_Of_Letters {
                         word += input;
                         break;
                 }
-
 
                 if (o && c && n) { // Когато сме срещнали и трите символа по веднъв влиза в if където печата и отново 'o'=false 'c'=false 'n'=false
                     System.out.print(word + " ");
