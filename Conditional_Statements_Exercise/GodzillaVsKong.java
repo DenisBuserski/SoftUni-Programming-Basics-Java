5c224ab3d6243d23c0664776ee96ac787189fd1c
Denis Buserski <denis.buserski@gmail.com>
2022-06-04 14:12:27 +0300

    Update GodzillaVsKong.java

--- Conditional_Statements_Exercise/GodzillaVsKong.java
+++ Conditional_Statements_Exercise/GodzillaVsKong.java
@@ -1,8 +1,6 @@
-package ConditionalStatementsExercise;
-
 import java.util.Scanner;
 
-public class GodzillaVsKong {
+public class Godzilla_Vs_Kong_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,32 +11,30 @@ public class GodzillaVsKong {
         double decor = budget * 0.10;
         double priceForClouths = numberWorkers * priceForWorker;
 
-        if(numberWorkers > 150){
+        if (numberWorkers > 150) {
             double priceForClouthsDiscount = priceForClouths - priceForClouths * 0.10;
             double priceForMovie = priceForClouthsDiscount + decor;
-            if(priceForMovie <= budget){
+            if (priceForMovie <= budget) {
                 double moneyLeft = budget - priceForMovie;
                 System.out.println("Action!");
                 System.out.printf("Wingard starts filming with %.2f leva left.", moneyLeft);
-            }
-            else if(priceForMovie > budget){
+            } else if (priceForMovie > budget) {
                 double noMoney = priceForMovie - budget;
                 System.out.println("Not enough money!");
                 System.out.printf("Wingard needs %.2f leva more.", noMoney);
             }
-        }
-        else if (numberWorkers < 150){
+        } else if (numberWorkers < 150) {
             double priceForMovie = priceForClouths + decor;
             if(priceForMovie <= budget){
                 double moneyLeft = budget - priceForMovie;
                 System.out.println("Action!");
                 System.out.printf("Wingard starts filming with %.2f leva left.", moneyLeft);
-            }
-            else if(priceForMovie > budget){
+            } else if (priceForMovie > budget) {
                 double noMoney = priceForMovie - budget;
                 System.out.println("Not enough money!");
                 System.out.printf("Wingard needs %.2f leva more.", noMoney);
             }
         }
+        
     }
 }
