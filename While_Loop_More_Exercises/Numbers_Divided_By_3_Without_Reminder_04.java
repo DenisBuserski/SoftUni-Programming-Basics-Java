61f7e69ae5681fa6938a5a1d9181c8da1f465fd6
Denis Buserski <denis.buserski@gmail.com>
2022-06-04 22:19:07 +0300

    Update and rename NumbersDividedBy3WithoutReminder.java to Numbers_Divided_By_3_Without_Reminder_04.java

--- While_Loop_More_Exercises/Numbers_Divided_By_3_Without_Reminder_04.java
+++ While_Loop_More_Exercises/Numbers_Divided_By_3_Without_Reminder_04.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Numbers_Divided_By_3_Without_Reminder {
+public class Numbers_Divided_By_3_Without_Reminder_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
