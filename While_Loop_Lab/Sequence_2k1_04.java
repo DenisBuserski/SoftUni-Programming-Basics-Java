e2ff47ec4e13fac1686c3ce71f5fcf596986c096
Denis Buserski <denis.buserski@gmail.com>
2022-06-05 12:20:26 +0300

    Update and rename Sequence2k1.java to Sequence_2k1_04.java

--- While_Loop_Lab/Sequence_2k1_04.java
+++ While_Loop_Lab/Sequence_2k1_04.java
@@ -1,18 +1,16 @@
-package WhileLoopLab;
-
 import java.util.Scanner;
 
-public class Sequence2k1 {
+public class Sequence_2k1_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         int k = 1;
 
         while (k <= n) {
             System.out.println(k);
             k = k * 2 + 1;
         }
+        
     }
 }
