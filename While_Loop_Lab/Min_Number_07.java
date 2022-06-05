9b96bf73f2078875a71407f7f3ed5705779d7a4e
Denis Buserski <denis.buserski@gmail.com>
2022-06-05 12:25:23 +0300

    Update and rename MinNumber.java to Min_Number_07.java

--- While_Loop_Lab/Min_Number_07.java
+++ While_Loop_Lab/Min_Number_07.java
@@ -1,8 +1,6 @@
-package WhileLoopLab;
-
 import java.util.Scanner;
 
-public class MinNumber {
+public class Min_Number_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,12 +11,11 @@ public class MinNumber {
             int number = Integer.parseInt(input);
             if (number < minNumber) {
                 minNumber = number;
-
             }
+            
             input = scanner.nextLine();
-
         }
+        
         System.out.println(minNumber);
-
     }
 }
