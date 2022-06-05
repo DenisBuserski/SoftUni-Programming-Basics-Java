aa816bfc2e009457e20c55cf404b9a18bf2d0c31
Denis Buserski <denis.buserski@gmail.com>
2022-06-05 12:28:07 +0300

    Update and rename GraduationPt2.java to Graduation_08.java

--- While_Loop_Lab/Graduation_08.java
+++ While_Loop_Lab/Graduation_08.java
@@ -1,21 +1,16 @@
-package WhileLoopLab;
-
 import java.util.Scanner;
 
-public class GraduationPt2 {
+public class Graduation_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String studentsName = scanner.nextLine();
-
         int badGradesCounter = 0;
         double grades = 0.0;
         int clazz = 0;
 
         while (clazz < 12) {
-
             double currentGrade = Double.parseDouble(scanner.nextLine());
-
             if (currentGrade < 4) {
                 badGradesCounter++;
                 if (badGradesCounter == 2) {
@@ -29,9 +24,9 @@ public class GraduationPt2 {
 
         if (clazz == 12) {
             System.out.printf("%s graduated. Average grade: %.2f", studentsName, grades / 12);
-        }
-        else {
+        } else {
             System.out.printf("%s has been excluded at %d grade", studentsName, ++clazz);
         }
+        
     }
 }
