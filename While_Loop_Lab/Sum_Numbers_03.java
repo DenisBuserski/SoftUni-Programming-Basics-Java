19819bae701cb099d6fdfde93c67eb574c42185c
Denis Buserski <denis.buserski@gmail.com>
2022-06-05 12:19:10 +0300

    Update and rename SumNumbers.java to Sum_Numbers_03.java

--- While_Loop_Lab/Sum_Numbers_03.java
+++ While_Loop_Lab/Sum_Numbers_03.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Sum_Numbers {
+public class Sum_Numbers_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
