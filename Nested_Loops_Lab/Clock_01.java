cff62fa11519cbb8ad055fe51430cfbb9799475d
Denis Buserski <denis.buserski@gmail.com>
2022-06-06 11:04:26 +0300

    Update and rename Clock.java to Clock_01.java

--- Nested_Loops_Lab/Clock_01.java
+++ Nested_Loops_Lab/Clock_01.java
@@ -1,14 +1,14 @@
-package NestedLoopsLab;
-
 import java.util.Scanner;
 
-public class Clock {
+public class Clock_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         for (int hours = 0; hours < 24; hours++) {
-            for (int min = 0; min < 60; min++)
-            System.out.printf("%d:%d%n", hours, min);
+            for (int min = 0; min < 60; min++) {
+                System.out.printf("%d:%d%n", hours, min);
+            }
         }
+        
     }
 }
