5d4871c5e8fbd1a954cc92f45445e2394a5dc7c2
Denis Buserski <denis.buserski@gmail.com>
2022-06-06 11:11:53 +0300

    Update and rename Travelling.java to Travelling_05.java

--- Nested_Loops_Lab/Travelling_05.java
+++ Nested_Loops_Lab/Travelling_05.java
@@ -1,18 +1,14 @@
-package NestedLoopsLab;
-
 import java.util.Scanner;
 
-public class Travelling {
+public class Travelling_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String destination = "";
         double totalSum = 0;
 
-
         while (!destination.equals("End")) {
             destination = scanner.nextLine();
-
             if (destination.equals("End")) {
                 break;
             }
@@ -23,16 +19,14 @@ public class Travelling {
             while (totalSum >= 0) {
                 money = Double.parseDouble(scanner.nextLine());
                 totalSum += money;
-
                 if (totalSum >= minBudget) {
                     System.out.printf("Going to %s!%n", destination);
                     totalSum = 0;
                     break;
                 }
-
             }
+            
         }
-
 
     }
 }
