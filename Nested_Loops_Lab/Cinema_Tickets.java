b0bbbf2fb3a63bc1b783c10cf5045996d78ddb00
Denis Buserski <denis.buserski@gmail.com>
2022-06-06 11:13:59 +0300

    Update and rename CinemaTickets.java to Cinema_Tickets.java

--- Nested_Loops_Lab/Cinema_Tickets.java
+++ Nested_Loops_Lab/Cinema_Tickets.java
@@ -1,8 +1,6 @@
-package NestedLoopsLab;
-
 import java.util.Scanner;
 
-public class CinemaTickets {
+public class Cinema_Tickets {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,7 +17,6 @@ public class CinemaTickets {
             String type = "";
             while (movieTickets < seats && !type.equals("End")) {
                 type = scanner.nextLine();
-
                 if (type.equals("student")) {
                     student++;
                     movieTickets++;
@@ -34,9 +31,10 @@ public class CinemaTickets {
                     totalTickets++;
                 }
             }
+            
             System.out.printf("%s - %.2f%% full.%n", movie, movieTickets * 100.0 / seats);
-
         }
+        
         System.out.printf("Total tickets: %d%n", totalTickets);
         System.out.printf("%.2f%% student tickets.%n", student * 100.0 / totalTickets);
         System.out.printf("%.2f%% standard tickets.%n", standard * 100.0 / totalTickets);
