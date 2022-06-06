83ba88ce3c5b711a6b5b7df3b00b2395bbc0cbac
Denis Buserski <denis.buserski@gmail.com>
2022-06-06 11:08:14 +0300

    Update and rename Combinations.java to Combinations_03.java

--- Nested_Loops_Lab/Combinations_03.java
+++ Nested_Loops_Lab/Combinations_03.java
@@ -1,27 +1,23 @@
-package NestedLoopsLab;
-
 import java.util.Scanner;
 
-public class Combinations {
+public class Combinations_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int number = Integer.parseInt(scanner.nextLine());
-
         int results = 0;
-
-        //number = a + b + c;
+        // number = a + b + c;
 
         for (int a = 0; a <= number; a++) {
             for (int b = 0; b <= number; b++) {
                 for (int c = 0; c <= number; c++) {
-
                     if (a + b + c == number) {
                         results++;
                     }
                 }
             }
         }
+        
         System.out.println(results);
     }
 }
