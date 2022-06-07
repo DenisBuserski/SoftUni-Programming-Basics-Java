8640a83752067c4365db91999d1c494a6ab2d4fc
Denis Buserski <denis.buserski@gmail.com>
2022-06-07 21:48:45 +0300

    Update and rename NumberPyramid.java to Number_Pyramid_01.java

--- Nested_Loops_Exercise/Number_Pyramid_01.java
+++ Nested_Loops_Exercise/Number_Pyramid_01.java
@@ -1,22 +1,17 @@
-package NestedLoopsExercise;
-
 import java.util.Scanner;
 
-public class NumberPyramid {
+public class Number_Pyramid_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n  = Integer.parseInt(scanner.nextLine());
-
         int number = 0;
         boolean isEquals = false;
 
         for (int rows = 1; rows <= n; rows++) {
-
             for (int cols = 1; cols <= rows; cols++) {
                 number++;
                 System.out.print(number + " ");
-
                 if (number >= n) {
                     isEquals = true;
                     break;
@@ -28,5 +23,6 @@ public class NumberPyramid {
 
             System.out.println();
         }
+        
     }
 }
