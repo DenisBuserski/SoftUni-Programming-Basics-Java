9c9acaaf2b4420b288a3aa6de80fdb90d627c0d4
Denis Buserski <denis.buserski@gmail.com>
2022-06-08 13:57:21 +0300

    Update and rename LettersCombinations.java to Letters_Combinations_02.java

--- Nested_Loops_More_Exercises/Letters_Combinations_02.java
+++ Nested_Loops_More_Exercises/Letters_Combinations_02.java
@@ -1,21 +1,17 @@
-package NestedLoopsMoreExercises;
-
 import java.util.Scanner;
 
-public class LettersCombinations {
+public class Letters_Combinations_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         char fromSymbol = scanner.nextLine().charAt(0);
         char toSymbol = scanner.nextLine().charAt(0);
         char toIgnore = scanner.nextLine().charAt(0);
-
         int counter = 0;
 
         for (char i = fromSymbol; i <= toSymbol; i++) {
             for (char j = fromSymbol; j <= toSymbol; j++) {
                 for (char k = fromSymbol; k <= toSymbol; k++) {
-
                     if (k != toIgnore && i != toIgnore && j != toIgnore) {
                         System.out.printf("%c%c%c ", i, j, k);
                         counter++;
@@ -23,11 +19,7 @@ public class LettersCombinations {
                 }
             }
         }
+        
         System.out.println(counter);
-
-
-
-
-
     }
 }
