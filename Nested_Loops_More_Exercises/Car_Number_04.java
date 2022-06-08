c2a500121d164f93bd41ad029e5b0cad675ecc6d
Denis Buserski <denis.buserski@gmail.com>
2022-06-08 13:58:50 +0300

    Update and rename CarNumber.java to Car_Number_04.java

--- Nested_Loops_More_Exercises/Car_Number_04.java
+++ Nested_Loops_More_Exercises/Car_Number_04.java
@@ -1,8 +1,6 @@
-package NestedLoopsMoreExercises;
-
 import java.util.Scanner;
 
-public class CarNumber {
+public class Car_Number_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,7 +11,6 @@ public class CarNumber {
             for (int b = start; b <= end; b++) {
                 for (int c = start; c <= end; c++) {
                     for (int d = start; d <= end; d++) {
-
                         if ((a % 2 == 0 && d % 2 != 0 && a > d && (b + c) % 2 == 0)
                                 || (a % 2 != 0 && d % 2 == 0 && a > d && (b + c) % 2 == 0)) {
                             System.out.printf("%d%d%d%d ", a, b, c, d);
