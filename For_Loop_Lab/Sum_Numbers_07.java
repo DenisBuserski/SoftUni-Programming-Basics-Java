3f52fc4dbeadf097b3fa697f6b5b2507b15b9a4c
Denis Buserski <denis.buserski@gmail.com>
2022-06-11 23:52:44 +0300

    Update Sum_Numbers_07.java

--- For_Loop_Lab/Sum_Numbers_07.java
+++ For_Loop_Lab/Sum_Numbers_07.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Sum_Numbers_)7 {
+public class Sum_Numbers_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
