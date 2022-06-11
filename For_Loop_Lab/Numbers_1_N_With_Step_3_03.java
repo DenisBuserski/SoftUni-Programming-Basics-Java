6f687949e2b5a3df71d589d78e2b42f638b449d4
Denis Buserski <denis.buserski@gmail.com>
2022-06-11 23:49:08 +0300

    Update and rename Numbers1NWithStep3.java to Numbers_1_N_With_Step_3_03.java

--- For_Loop_Lab/Numbers_1_N_With_Step_3_03.java
+++ For_Loop_Lab/Numbers_1_N_With_Step_3_03.java
@@ -1,15 +1,13 @@
-package ForLoopLab;
-
 import java.util.Scanner;
 
-public class Numbers1NWithStep3 {
+public class Numbers_1_N_With_Step_3_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         for (int i = 1; i <= n; i += 3){
             System.out.println(i);
         }
+        
     }
 }
