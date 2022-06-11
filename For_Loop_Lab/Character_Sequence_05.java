a648a3a980057c4bba920b438de36225cc3f9785
Denis Buserski <denis.buserski@gmail.com>
2022-06-11 23:50:52 +0300

    Update and rename CharacterSequence.java to Character_Sequence_05.java

--- For_Loop_Lab/Character_Sequence_05.java
+++ For_Loop_Lab/Character_Sequence_05.java
@@ -1,17 +1,15 @@
-package ForLoopLab;
-
 import java.util.Scanner;
 
-public class CharacterSequence {
+public class Character_Sequence_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String text = scanner.nextLine();
-
         int length = text.length();
 
         for (int i = 0; i < text.length(); i++) {
             System.out.println(text.charAt(i));
         }
+        
     }
 }
