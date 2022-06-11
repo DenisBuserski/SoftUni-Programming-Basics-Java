6c169638dc5862aba01a5a1f35f79aed8f7e1f12
Denis Buserski <denis.buserski@gmail.com>
2022-06-11 23:55:11 +0300

    Update and rename NumberSequence.java to Number_Sequence_08.java

--- For_Loop_Lab/Number_Sequence_08.java
+++ For_Loop_Lab/Number_Sequence_08.java
@@ -1,19 +1,15 @@
-package ForLoopLab;
-
 import java.util.Scanner;
 
-public class NumberSequence {
+public class Number_Sequence_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         int maxNumber = Integer.MIN_VALUE;
         int minNumber = Integer.MAX_VALUE;
 
         for (int i = 1; i <= n; i += 1) {
             int number = Integer.parseInt(scanner.nextLine());
-
             if (number > maxNumber) {
                 maxNumber = number;
             }
@@ -21,6 +17,7 @@ public class NumberSequence {
                 minNumber = number;
             }
         }
+        
         System.out.printf("Max number: %d%n", maxNumber);
         System.out.printf("Min number: %d", minNumber);
     }
