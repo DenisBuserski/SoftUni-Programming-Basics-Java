8a7694080a6808dafe69edb5baa25ba644a18923
Denis Buserski <denis.buserski@gmail.com>
2022-06-11 23:50:16 +0300

    Update and rename EvenPowersOf2.java to Even_Powers_Of_2_04.java

--- For_Loop_Lab/Even_Powers_Of_2_04.java
+++ For_Loop_Lab/Even_Powers_Of_2_04.java
@@ -1,22 +1,14 @@
-package ForLoopLab;
-
 import java.util.Scanner;
 
-public class EvenPowersOf2 {
+public class Even_Powers_Of_2_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         for (int i = 0; i <= n; i += 2) {
             double number = Math.pow(2, i);
             System.out.printf("%.0f%n", number);
         }
-
-
-
-
-
 
     }
 }
