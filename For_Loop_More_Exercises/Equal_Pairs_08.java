14400abfa60639788d2bc7263c8e52b2b459bd32
Denis Buserski <denis.buserski@gmail.com>
2022-06-12 21:44:49 +0300

    Update and rename EqualPairs.java to Equal_Pairs_08.java

--- For_Loop_More_Exercises/Equal_Pairs_08.java
+++ For_Loop_More_Exercises/Equal_Pairs_08.java
@@ -1,23 +1,18 @@
-package ForLoopMoreExercises;
-
 import java.util.Scanner;
 
-public class EqualPairs {
+public class Equal_Pairs_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         int firstSum = 0;
         int midSum = 0;
         int diff1 = 0;
-
         boolean check = true;
 
         for (int i = 1; i <= n; i++) {
             int numA = Integer.parseInt(scanner.nextLine());
             int numB = Integer.parseInt(scanner.nextLine());
-
             if (i % 2 != 0) {
                 firstSum = numA + numB;
             } else {
@@ -29,18 +24,14 @@ public class EqualPairs {
             } else {
                 diff1 = Math.abs(midSum - firstSum);
                 check = false;
-
             }
-
         }
 
         if (check) {
             System.out.printf("Yes, value=%d", firstSum);
         } else {
-
             System.out.printf("No, maxdiff=%d", diff1);
         }
-
 
     }
 }
