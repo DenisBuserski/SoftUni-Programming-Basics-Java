9a59950b13e9aac8cd1d39857037cc3906f3e3a0
Denis Buserski <denis.buserski@gmail.com>
2022-06-12 21:34:27 +0300

    Update and rename Grades.java to Grades_04.java

--- For_Loop_More_Exercises/Grades_04.java
+++ For_Loop_More_Exercises/Grades_04.java
@@ -1,8 +1,6 @@
-package ForLoopMoreExercises;
-
 import java.util.Scanner;
 
-public class Grades {
+public class Grades_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,27 +9,21 @@ public class Grades {
         double veryGood = 0;
         double good = 0;
         double fail = 0;
-
         double avarage = 0;
-
 
         for (int i = 1; i <= students; i++) {
             double grade = Double.parseDouble(scanner.nextLine());
             if (grade >= 5.00) {
                 topStudents++;
-            }
-            else if (grade >= 4.00) {
+            } else if (grade >= 4.00) {
                 veryGood++;
-            }
-            else if (grade >= 3.00) {
+            } else if (grade >= 3.00) {
                 good++;
-            }
-            else {
+            } else {
                 fail++;
             }
 
             avarage += grade;
-
         }
 
         System.out.printf("Top students: %.2f%%%n", (topStudents / students) * 100);
@@ -39,8 +31,5 @@ public class Grades {
         System.out.printf("Between 3.00 and 3.99: %.2f%%%n", (good / students) * 100);
         System.out.printf("Fail: %.2f%%%n", (fail / students) * 100);
         System.out.printf("Average: %.2f", avarage / students);
-
-
-
     }
 }
