a98dbb1830efc19cf2e8be73231b56b1aead03ea
Denis Buserski <denis.buserski@gmail.com>
2022-06-15 21:54:26 +0300

    Update and rename DivideWithoutRemainder.java to Divide_Without_Remainder.java

--- For_Loop_Exercise/Divide_Without_Remainder.java
+++ For_Loop_Exercise/Divide_Without_Remainder.java
@@ -1,20 +1,16 @@
-package ForLoopExercise;
-
 import java.util.Scanner;
 
-public class DivideWithoutRemainder {
+public class Divide_Without_Remainder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         double p1 = 0.0;
         double p2 = 0.0;
         double p3 = 0.0;
 
         for (int i = 1; i <= n; i++) {
             int number = Integer.parseInt(scanner.nextLine());
-
             if (number % 2 == 0) {
                 p1++;
             }
@@ -29,7 +25,5 @@ public class DivideWithoutRemainder {
         System.out.printf("%.2f%%%n", (p1 / n) * 100);
         System.out.printf("%.2f%%%n", (p2 / n) * 100);
         System.out.printf("%.2f%%", (p3 / n) * 100);
-
-
     }
 }
