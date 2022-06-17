7ecda45ebd716636b9f9c2b48b330f888ff5b07c
Denis Buserski <denis.buserski@gmail.com>
2022-06-17 23:18:09 +0300

    Update and rename MetricConverter.java to Metric_Converter.java

--- Conditional_Statements_Exercise/Metric_Converter.java
+++ Conditional_Statements_Exercise/Metric_Converter.java
@@ -1,8 +1,6 @@
-package ConditionalStatementsExercise;
-
 import java.util.Scanner;
 
-public class MetricConverter {
+public class Metric_Converter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,39 +8,35 @@ public class MetricConverter {
         String textIn = scanner.nextLine();
         String textOut = scanner.nextLine();
 
-        //double m = 1;
-        //double mm = 1000;
-        //double cm = 100;
+        // double m = 1;
+        // double mm = 1000;
+        // double cm = 100;
 
-        if(textIn.equals("m")){
-            if(textOut.equals("mm")){
+        if (textIn.equals("m")){
+            if (textOut.equals("mm")){
                 double result = number * 1000;
                 System.out.printf("%.3f", result);
-            }
-            else if (textOut.equals("cm")){
+            } else if (textOut.equals("cm")){
                 double result = number * 100;
                 System.out.printf("%.3f", result);
             }
-        }
-        else if (textIn.equals("mm")){
-            if(textOut.equals("m")){
+        } else if (textIn.equals("mm")){
+            if (textOut.equals("m")){
                 double result = number / 1000;
                 System.out.printf("%.3f", result);
-            }
-            else if(textOut.equals("cm")){
+            } else if (textOut.equals("cm")){
                 double result = number / 10;
                 System.out.printf("%.3f", result);
             }
-        }
-        else if(textIn.equals("cm")){
-            if(textOut.equals("m")){
+        } else if (textIn.equals("cm")){
+            if (textOut.equals("m")){
                 double result = number / 100;
                 System.out.printf("%.3f", result);
-            }
-            else if (textOut.equals("mm")){
+            } else if (textOut.equals("mm")){
                 double result = number * 10;
                 System.out.printf("%.3f", result);
             }
         }
+        
     }
 }
