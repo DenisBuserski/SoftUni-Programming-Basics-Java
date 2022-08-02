f9a0874998ba43162ad44d14061b7fc5bd2ebdc7
Denis Buserski <denis.buserski@gmail.com>
2022-08-02 06:59:00 +0300

    Add files via upload

---
+++ Java_Basics_Exam_1_June_2014/Logs_Aggregator_04.java
@@ -0,0 +1,44 @@
+package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Archive.Advanced_Java.Advanced_Java_Exams.Java_Basics_Exam_1_June_2014;
+
+import java.util.*;
+
+public class Logs_Aggregator_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Integer> durationData = new TreeMap<>();
+        Map<String, TreeSet<String> > ipInfo = new TreeMap<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+        while (n-- > 0) {
+            String [] line = scanner.nextLine().split("\\s+");
+            String IP = line[0];
+            String user = line[1];
+            int duration = Integer.parseInt(line[2]);
+
+            if (!durationData.containsKey(user)) {
+                durationData.put(user, duration);
+            } else {
+                int oldDuration = durationData.get(user);
+                durationData.put(user, oldDuration + duration);
+            }
+
+            if (!ipInfo.containsKey(user)) {
+                ipInfo.put(user, new TreeSet<>());
+                ipInfo.get(user).add(IP);
+            } else {
+                ipInfo.get(user).add(IP);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : durationData.entrySet()) {
+            System.out.print(entry.getKey() + ": " + entry.getValue() + " ");
+            for (Map.Entry<String, TreeSet<String>> entry2 : ipInfo.entrySet()) {
+                if (entry2.getKey().equals(entry.getKey())) {
+                    System.out.println(entry2.getValue());
+                }
+            }
+        }
+
+    }
+}
---
+++ Java_Basics_Exam_1_June_2014/Stuck_Numbers_01.java
@@ -0,0 +1,39 @@
+package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Archive.Advanced_Java.Advanced_Java_Exams.Java_Basics_Exam_1_June_2014;
+
+import java.util.Scanner;
+
+public class Stuck_Numbers_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        String [] numbers = new String[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.next();
+        }
+
+        int count = 0;
+        for (int a = 0; a < numbers.length; a++) {
+            for (int b = 0; b < numbers.length; b++) {
+                for (int c = 0; c < numbers.length; c++) {
+                    for (int d = 0; d < numbers.length; d++) {
+
+                        if (!numbers[a].equals(numbers[b]) && !numbers[a].equals(numbers[c]) && !numbers[a].equals(numbers[d]) &&
+                                !numbers[b].equals(numbers[c]) && !numbers[b].equals(numbers[d]) && !numbers[c].equals(numbers[d])) {
+
+                            if (numbers[a].concat(numbers[b]).equals(numbers[c].concat(numbers[d]))) {
+                                System.out.printf("%s|%s==%s|%s%n", numbers[a], numbers[b], numbers[c], numbers[d]);
+                                count++;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        if (count == 0) {
+            System.out.println("No");
+        }
+
+    }
+}
