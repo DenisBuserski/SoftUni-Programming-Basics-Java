5df8648ddc45d58a862d72bb9befed1310d6bcc7
Denis Buserski <denis.buserski@gmail.com>
2022-08-02 07:03:02 +0300

    Add files via upload

---
+++ Java_Basics_Exam_8_February_2015/Gandalfs_Stash_01.java
@@ -0,0 +1,52 @@
+package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Archive.Advanced_Java.Advanced_Java_Exams.Java_Basics_Exam_8_February_2015;
+
+import java.util.Scanner;
+
+public class Gandalfs_Stash_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startingMood = Integer.parseInt(scanner.nextLine());
+        String [] input = scanner.nextLine().split("[^a-zA-Z]+\\s*");
+
+
+        for (String food : input) {
+
+            switch (food.toLowerCase()) {
+                case "cram":
+                    startingMood += 2;
+                    break;
+                case "lembas":
+                    startingMood += 3;
+                    break;
+                case "apple":
+                case "melon":
+                    startingMood += 1;
+                    break;
+                case "honeycake":
+                    startingMood += 5;
+                    break;
+                case "mushrooms":
+                    startingMood -= 10;
+                    break;
+                default:
+                    startingMood -= 1;
+                    break;
+
+            }
+
+        }
+
+        System.out.println(startingMood);
+        if (startingMood < -5) {
+            System.out.println("Angry");
+        } else if (startingMood >= -5 && startingMood <= 0) {
+            System.out.println("Sad");
+        } else if (startingMood >= 0 && startingMood <= 15) {
+            System.out.println("Happy");
+        } else if (startingMood > 15) {
+            System.out.println("Special JavaScript mood");
+        }
+
+    }
+}
---
+++ Java_Basics_Exam_8_February_2015/Letters_Change_Numbers_02.java
@@ -0,0 +1,44 @@
+package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Archive.Advanced_Java.Advanced_Java_Exams.Java_Basics_Exam_8_February_2015;
+
+import java.util.Scanner;
+
+public class Letters_Change_Numbers_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] words = scanner.nextLine().split("\\s+");
+        double totalSum = 0;
+
+        for (String entry : words) {
+            String [] letters = entry.split("[0-9]+");
+            String firstLetter = letters[0];
+            String lastLetter = letters[1];
+            String [] numbers = entry.split("[A-z]+");
+            double number = Double.parseDouble(numbers[1]);
+            double currentSum = 0;
+            int firstLetterNum = 0;
+            int LastLetterNum = 0;
+
+            if (firstLetter.charAt(0) >= 65 && firstLetter.charAt(0) <= 90) { // First letter - Upper
+                firstLetterNum = firstLetter.charAt(0) - 64;
+                currentSum = number / firstLetterNum;
+            } else if (firstLetter.charAt(0) >= 97 && firstLetter.charAt(0) <= 122) { // First letter Lower
+                firstLetterNum = firstLetter.charAt(0) - 96;
+                currentSum = number * firstLetterNum;
+            }
+
+
+            if (lastLetter.charAt(0) >= 65 && lastLetter.charAt(0) <= 90) { // Last letter - Upper
+                LastLetterNum = lastLetter.charAt(0) - 64;
+                currentSum -= LastLetterNum;
+            } else if (lastLetter.charAt(0) >= 97 && lastLetter.charAt(0) <= 122) { // Last letter Lower
+                LastLetterNum = lastLetter.charAt(0) - 96;
+                currentSum += LastLetterNum;
+            }
+            totalSum += currentSum;
+
+        }
+
+        System.out.printf("%.2f", totalSum);
+    }
+}
