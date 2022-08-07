0ec21150aa86fa8372dea3b794d7b1b6f144b135
Denis Buserski <denis.buserski@gmail.com>
2022-08-07 19:24:39 +0300

    Update Concatenate_Data_06.java

--- First_Steps_In_Coding_Lab/Concatenate_Data_06.java
+++ First_Steps_In_Coding_Lab/Concatenate_Data_06.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class Concatenate_Data_06 {
     public static void main(String[] args) {
-        Scanner scr = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        String firstName = scr.nextLine();
-        String lastName = scr.nextLine();
-        int age = Integer.parseInt(scr.nextLine());
-        String town = scr.nextLine();
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String town = scanner.nextLine();
 
         System.out.printf("You are %s %s, a %d-years old person from %s.", firstName, lastName, age, town );
-
         // System.out.println("You are" + firstName + " " + lastName + ", a" + age + "-years old person from" + town + ".");
     }
 }
