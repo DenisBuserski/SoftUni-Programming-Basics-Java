da865c644b1720fac4abd334016215adf5ba2090
Denis Buserski <denis.buserski@gmail.com>
2022-08-08 08:31:04 +0300

    Create Circle_Area_And_Perimeter_08.java

---
+++ First_Steps_In_Coding_More_Exercise/Circle_Area_And_Perimeter_08.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Circle_Area_And_Perimeter_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double r = Double.parseDouble(scanner.nextLine());
+
+        double lice = r * r * Math.PI;
+        double  perimetar = 2 * r * Math.PI;
+
+        System.out.printf("%.2f", lice);
+        System.out.println();
+        System.out.printf("%.2f", perimetar);
+    }
+}
