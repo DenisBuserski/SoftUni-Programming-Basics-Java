837daea7b310ea8cd5d3a7362600ded67db367aa
Denis Buserski <denis.buserski@gmail.com>
2022-08-08 08:25:39 +0300

    Create Celsius_To_Fahrenheit_03.java

---
+++ First_Steps_In_Coding_More_Exercise/Celsius_To_Fahrenheit_03.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Celsius_To_Fahrenheit_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double gradusiC = Double.parseDouble(scanner.nextLine());
+
+        double gradusiF = gradusiC * 1.8 + 32;
+
+        System.out.printf("%.2f", gradusiF);
+    }
+}
