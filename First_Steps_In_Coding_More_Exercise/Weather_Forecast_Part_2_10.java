faa3215f4bb5de38cfa207f07ba38ac13c8423bd
Denis Buserski <denis.buserski@gmail.com>
2022-08-09 06:45:35 +0300

    Create Weather_Forecast_Part_2_10.java

---
+++ First_Steps_In_Coding_More_Exercise/Weather_Forecast_Part_2_10.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Weather_Forecast_Part_2_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = Double.parseDouble(scanner.nextLine());
+
+        if (a <= 5.00) {
+            System.out.println("unknown");
+        } else if (a <= 11.9){
+            System.out.println("Cold");
+        } else if (a <= 14.9){
+            System.out.println("Cool");
+        } else if (a <= 20.00){
+            System.out.println("Mild");
+        } else if (a <= 25.9){
+            System.out.println("Warm");
+        } else if (a <= 35.00){
+            System.out.println("Hot");
+        } else if (a > 35.00){
+            System.out.println("unknown");
+        }
+    }
+}
