57a91f783e3e097e4f409b2ce758f2f8f501e74b
Denis Buserski <denis.buserski@gmail.com>
2022-08-09 06:52:28 +0300

    Update and rename EvenOrOdd.java to Even_Or_Odd_03.java

--- Conditional_Statements_Lab/Even_Or_Odd_03.java
+++ Conditional_Statements_Lab/Even_Or_Odd_03.java
@@ -1,16 +1,14 @@
-package ConditionalStatementsLab;
-
 import java.util.Scanner;
 
-public class EvenOrOdd {
+public class Even_Or_Odd_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int number = Integer.parseInt(scanner.nextLine());
 
-        if (number % 2 == 0){
+        if (number % 2 == 0) {
             System.out.println("even");
-        } else{
+        } else {
             System.out.println("odd");
         }
     }
