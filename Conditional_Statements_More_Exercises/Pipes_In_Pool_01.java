0b09e4cc9994d326c06aaa448efb67fa9ded3aa2
Denis Buserski <denis.buserski@gmail.com>
2022-08-10 22:20:46 +0300

    Update and rename PipesInPool.java to Pipes_In_Pool_01.java

--- Conditional_Statements_More_Exercises/Pipes_In_Pool_01.java
+++ Conditional_Statements_More_Exercises/Pipes_In_Pool_01.java
@@ -1,8 +1,6 @@
-package ConditionalStatementsMoreExercises;
-
 import java.util.Scanner;
 
-public class PipesInPool {
+public class Pipes_In_Pool_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -21,11 +19,8 @@ public class PipesInPool {
             double result1 = (p1Pool / total) * 100;
             double result2 = (p2Pool / total) * 100;
             System.out.printf("The pool is %.2f %% full. Pipe 1: %.2f %%. Pipe 2: %.2f %%.", result, result1, result2);
-        }
-        else if(total > capacityV){
-
+        } else if(total > capacityV){
             System.out.printf("For %.2f the pool overflows with %.2f liters.", hours, litersOut);
-
         }
 
     }
