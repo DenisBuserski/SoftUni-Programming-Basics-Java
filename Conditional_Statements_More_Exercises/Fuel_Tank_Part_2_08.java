ab561cfcfa5b27e57341001688ad5a4465518f1a
Denis Buserski <denis.buserski@gmail.com>
2022-08-10 22:32:55 +0300

    Update and rename FuelTankPart2.java to Fuel_Tank_Part_2_08.java

--- Conditional_Statements_More_Exercises/Fuel_Tank_Part_2_08.java
+++ Conditional_Statements_More_Exercises/Fuel_Tank_Part_2_08.java
@@ -1,8 +1,6 @@
-package ConditionalStatementsMoreExercises;
-
 import java.util.Scanner;
 
-public class FuelTankPart2 {
+public class Fuel_Tank_Part_2_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -30,36 +28,32 @@ public class FuelTankPart2 {
                 if (fuelForCar.equals("Gasoline")){ System.out.printf("%.2f lv.", gasolineWithD - (gasolineWithD * 0.08)); }
                 if (fuelForCar.equals("Diesel")){ System.out.printf("%.2f lv.", dieselWithD - (dieselWithD * 0.08)); }
                 if (fuelForCar.equals("Gas")){ System.out.printf("%.2f lv.", gasWithD - (gasWithD * 0.08)); }
-            }
-            else if (card.equals("No")) {
+            } else if (card.equals("No")) {
                 if (fuelForCar.equals("Gasoline")) { System.out.printf("%.2f lv.", gasolineWithoutD - (gasolineWithoutD * 0.08)); }
                 if (fuelForCar.equals("Diesel")) { System.out.printf("%.2f lv.", dieselWithoutD - (dieselWithoutD * 0.08)); }
                 if (fuelForCar.equals("Gas")) { System.out.printf("%.2f lv.", gasWithoutD - (gasWithoutD * 0.08)); }
             }
-        }
-        else if (fuel > 25) {
+        } else if (fuel > 25) {
             if (card.equals("Yes")) {
                 if (fuelForCar.equals("Gasoline")) { System.out.printf("%.2f lv.", gasolineWithD - (gasolineWithD * 0.10)); }
                 if (fuelForCar.equals("Diesel")) { System.out.printf("%.2f lv.", dieselWithD - (dieselWithD * 0.10)); }
                 if (fuelForCar.equals("Gas")) { System.out.printf("%.2f lv.", gasWithD - (gasWithD * 0.10)); }
-            }
-            else if (card.equals("No")) {
+            } else if (card.equals("No")) {
                 if (fuelForCar.equals("Gasoline")) { System.out.printf("%.2f lv.", gasolineWithoutD - (gasolineWithoutD * 0.10)); }
                 if (fuelForCar.equals("Diesel")) { System.out.printf("%.2f lv.", dieselWithoutD - (dieselWithoutD * 0.10)); }
                 if (fuelForCar.equals("Gas")) { System.out.printf("%.2f lv.", gasWithoutD - (gasWithoutD * 0.10)); }
             }
-        }
-        else if (fuel < 20){
+        } else if (fuel < 20){
             if (card.equals("Yes")) {
                 if (fuelForCar.equals("Gasoline")) { System.out.printf("%.2f lv.", gasolineWithD); }
                 if (fuelForCar.equals("Diesel")) { System.out.printf("%.2f lv.", dieselWithD); }
                 if (fuelForCar.equals("Gas")) { System.out.printf("%.2f lv.", gasWithD); }
-            }
-            else if (card.equals("No")) {
+            } else if (card.equals("No")) {
                 if (fuelForCar.equals("Gasoline")) { System.out.printf("%.2f lv.", gasolineWithoutD); }
                 if (fuelForCar.equals("Diesel")) { System.out.printf("%.2f lv.", dieselWithoutD); }
                 if (fuelForCar.equals("Gas")) { System.out.printf("%.2f lv.", gasWithoutD); }
             }
         }
+        
     }
 }
