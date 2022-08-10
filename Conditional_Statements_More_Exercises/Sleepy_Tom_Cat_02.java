6500464706be8eb60bd66ff706fbd8d44dfc65f5
Denis Buserski <denis.buserski@gmail.com>
2022-08-10 22:21:49 +0300

    Update and rename SleepyTomCat.java to Sleepy_Tom_Cat_02.java

--- Conditional_Statements_More_Exercises/Sleepy_Tom_Cat_02.java
+++ Conditional_Statements_More_Exercises/Sleepy_Tom_Cat_02.java
@@ -1,13 +1,10 @@
-package ConditionalStatementsMoreExercises;
-
 import java.util.Scanner;
 
-public class SleepyTomCat {
+public class Sleepy_Tom_Cat_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int restDaysNum = Integer.parseInt(scanner.nextLine());
-
         double playRest = restDaysNum * 127;
         double playWork = (365 - restDaysNum) * 63;
         double totalPlay = playRest + playWork;
@@ -18,13 +15,13 @@ public class SleepyTomCat {
             double minutes = result % 60;
             System.out.println("Tom sleeps well");
             System.out.printf("%.0f hours and %.0f minutes less for play", hours, minutes);
-        }
-        else if (totalPlay > 30000){
+        } else if (totalPlay > 30000){
             double result  = totalPlay - 30000;
             double hours = Math.floor(result / 60);
             double minutes = result % 60;
             System.out.println("Tom will run away");
             System.out.printf("%.0f hours and %.0f minutes more for play", hours, minutes);
         }
+        
     }
 }
