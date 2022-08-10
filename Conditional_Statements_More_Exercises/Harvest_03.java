d923d23940ae4af447adbe5807cc43909418d981
Denis Buserski <denis.buserski@gmail.com>
2022-08-10 22:22:50 +0300

    Update and rename Harvest.java to Harvest_03.java

--- Conditional_Statements_More_Exercises/Harvest_03.java
+++ Conditional_Statements_More_Exercises/Harvest_03.java
@@ -1,8 +1,6 @@
-package ConditionalStatementsMoreExercises;
-
 import java.util.Scanner;
 
-public class Harvest {
+public class Harvest_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -14,16 +12,16 @@ public class Harvest {
         double totalGrape = grapeArea * grapeFor1SquareM;
         double wine = (0.40 * totalGrape) / 2.5;
 
-        if(wine >= wineNeeded){
+        if (wine >= wineNeeded){
             double wineLeft = wine - wineNeeded;
             double wineForWorkers = Math.ceil(wineLeft / workersNum);
             System.out.printf("Good harvest this year! Total wine: %.0f liters.", Math.floor(wine));
             System.out.println();
             System.out.printf("%.0f liters left -> %.0f liters per person.", wineLeft, wineForWorkers);
-        }
-        else if (wine < wineNeeded){
+        } else if (wine < wineNeeded){
             double wineLeft1 = Math.floor(wineNeeded - wine);
             System.out.printf("It will be a tough winter! More %.0f liters wine needed.", wineLeft1);
         }
+        
     }
 }
