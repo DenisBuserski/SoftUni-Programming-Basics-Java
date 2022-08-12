1b931c2cb6bf46f3f2fcd57149d6c37e26d1eb6b
Denis Buserski <denis.buserski@gmail.com>
2022-08-12 22:02:33 +0300

    Create Numbers_From_1_To_10_09.java

---
+++ Conditional_Statements_Advanced_More_Exercises/Numbers_From_1_To_10_09.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Numbers_From_1_To_10_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
