1044434dd0f3faace2e7ec08728223cb28ba418e
Denis Buserski <denis.buserski@gmail.com>
2022-09-19 06:45:33 +0300

    Update Birthday_Party.java

--- First_Steps_In_Coding/Exercise/Birthday_Party.java
+++ First_Steps_In_Coding/Exercise/Birthday_Party.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Birthday_Party {
+public class Birthday_Party_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
