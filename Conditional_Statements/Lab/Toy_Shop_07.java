702b684ce399a62e3c701f88dd391653a4a01261
Denis Buserski <denis.buserski@gmail.com>
2022-09-20 07:50:08 +0300

    Update and rename Toy_Shop.java to Toy_Shop_07.java

--- Conditional_Statements/Lab/Toy_Shop_07.java
+++ Conditional_Statements/Lab/Toy_Shop_07.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Toy_Shop {
+public class Toy_Shop_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -14,22 +14,22 @@ public class Toy_Shop {
         double toysPrice =  puzzles * 2.60 + dolls * 3.00 + bears * 4.10 + minions * 8.20 + trucks * 2.00;
         int toys = puzzles + dolls + bears + minions + trucks;
 
-        if (toys >= 50){
+        if (toys >= 50) {
             double priceAfterDiscount = toysPrice - toysPrice * 0.25;
             double rent = priceAfterDiscount - priceAfterDiscount * 0.10;
-            if (rent >= tripPrice){
+            if (rent >= tripPrice) {
                 double result = rent - tripPrice;
                 System.out.printf("Yes! %.2f lv left.", result);
-            } else if (rent < tripPrice){
+            } else if (rent < tripPrice) {
                 double result = tripPrice - rent;
                 System.out.printf("Not enough money! %.2f lv needed.", result);
             }
-        } else if (toys < 49){
+        } else if (toys < 49) {
             double rent = toysPrice - toysPrice * 0.10;
-            if (rent >= tripPrice){
+            if (rent >= tripPrice) {
                 double result = rent - tripPrice;
                 System.out.printf("Yes! %.2f lv left.", result);
-            } else if(rent < tripPrice){
+            } else if(rent < tripPrice) {
                 double result = tripPrice - rent;
                 System.out.printf("Not enough money! %.2f lv needed.", result);
             }
