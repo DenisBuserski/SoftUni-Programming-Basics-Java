3347d40f07791e0ee63bd72a8f40baef018c005b
Denis Buserski <denis.buserski@gmail.com>
2022-09-21 20:49:01 +0300

    Update and rename World_Swimming_Record_06.java to World_Swimming_Record_07.java

--- Conditional_Statements/Exercise/World_Swimming_Record_07.java
+++ Conditional_Statements/Exercise/World_Swimming_Record_07.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class World_Swimming_Record_06 {
+public class World_Swimming_Record_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
