4f54476d160194426d729ba80ca8a0209bfb921f
Denis Buserski <denis.buserski@gmail.com>
2022-09-21 20:46:26 +0300

    Update and rename Speed_Info.java to Speed_Info_03.java

--- Conditional_Statements/Exercise/Speed_Info_03.java
+++ Conditional_Statements/Exercise/Speed_Info_03.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Speed_Info {
+public class Speed_Info_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
