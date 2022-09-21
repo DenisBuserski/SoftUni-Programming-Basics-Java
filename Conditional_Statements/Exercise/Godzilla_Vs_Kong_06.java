4b2b6320a4c0befe41f11e652bc44abcce13e1c9
Denis Buserski <denis.buserski@gmail.com>
2022-09-21 20:48:19 +0300

    Update and rename Godzilla_Vs_Kong_05.java to Godzilla_Vs_Kong_06.java

--- Conditional_Statements/Exercise/Godzilla_Vs_Kong_06.java
+++ Conditional_Statements/Exercise/Godzilla_Vs_Kong_06.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Godzilla_Vs_Kong_05 {
+public class Godzilla_Vs_Kong_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
