cbb37070c8e296c70692e9b0c59281ba5dec8a6e
Denis Buserski <denis.buserski@gmail.com>
2022-09-21 20:47:13 +0300

    Update and rename Metric_Converter.java to Metric_Converter_04.java

--- Conditional_Statements/Exercise/Metric_Converter_04.java
+++ Conditional_Statements/Exercise/Metric_Converter_04.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Metric_Converter {
+public class Metric_Converter_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
