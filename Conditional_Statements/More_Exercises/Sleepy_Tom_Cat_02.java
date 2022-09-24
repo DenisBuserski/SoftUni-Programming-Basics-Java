ff1bf2b85dcd2ccf79d5e963834562936ce4ec70
Denis Buserski <denis.buserski@gmail.com>
2022-09-24 22:26:59 +0300

    Update and rename Conditional_Statements_More_Exercises/Sleepy_Tom_Cat_02.java to Conditional_Statements/More_Exercises/Sleepy_Tom_Cat_02.java

--- Conditional_Statements/More_Exercises/Sleepy_Tom_Cat_02.java
+++ Conditional_Statements/More_Exercises/Sleepy_Tom_Cat_02.java
@@ -9,13 +9,13 @@ public class Sleepy_Tom_Cat_02 {
         double playWork = (365 - restDaysNum) * 63;
         double totalPlay = playRest + playWork;
 
-        if (totalPlay <= 30000){
+        if (totalPlay <= 30000) {
             double result = 30000 - totalPlay;
             double hours = Math.floor(result / 60);
             double minutes = result % 60;
             System.out.println("Tom sleeps well");
             System.out.printf("%.0f hours and %.0f minutes less for play", hours, minutes);
-        } else if (totalPlay > 30000){
+        } else if (totalPlay > 30000) {
             double result  = totalPlay - 30000;
             double hours = Math.floor(result / 60);
             double minutes = result % 60;
