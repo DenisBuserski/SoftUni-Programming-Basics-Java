8b28cab5a338d658f4651caf6e5add7acd259bdc
Denis Buserski <denis.buserski@gmail.com>
2022-09-24 22:27:41 +0300

    Update and rename Conditional_Statements_More_Exercises/Harvest_03.java to Conditional_Statements/More_Exercises/Harvest_03.java

--- Conditional_Statements/More_Exercises/Harvest_03.java
+++ Conditional_Statements/More_Exercises/Harvest_03.java
@@ -12,13 +12,13 @@ public class Harvest_03 {
         double totalGrape = grapeArea * grapeFor1SquareM;
         double wine = (0.40 * totalGrape) / 2.5;
 
-        if (wine >= wineNeeded){
+        if (wine >= wineNeeded) {
             double wineLeft = wine - wineNeeded;
             double wineForWorkers = Math.ceil(wineLeft / workersNum);
             System.out.printf("Good harvest this year! Total wine: %.0f liters.", Math.floor(wine));
             System.out.println();
             System.out.printf("%.0f liters left -> %.0f liters per person.", wineLeft, wineForWorkers);
-        } else if (wine < wineNeeded){
+        } else if (wine < wineNeeded) {
             double wineLeft1 = Math.floor(wineNeeded - wine);
             System.out.printf("It will be a tough winter! More %.0f liters wine needed.", wineLeft1);
         }
