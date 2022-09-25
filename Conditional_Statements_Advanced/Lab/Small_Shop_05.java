b3d2ea69bae5f6e5761ea320682804945e1040da
Denis Buserski <denis.buserski@gmail.com>
2022-09-25 13:07:44 +0300

    Update and rename Conditional_Statements_Advanced_Lab/Small_Shop_05.java to Conditional_Statements_Advanced/Lab/Small_Shop_05.java

--- Conditional_Statements_Advanced/Lab/Small_Shop_05.java
+++ Conditional_Statements_Advanced/Lab/Small_Shop_05.java
@@ -14,39 +14,39 @@ public class Small_Shop_05 {
             case "Sofia":
                 if (product.equals("coffee")) {
                     price = 0.50;
-                } else if (product.equals("water")){
+                } else if (product.equals("water")) {
                     price = 0.80;
                 } else if (product.equals("beer")) {
                     price = 1.20;
                 } else if (product.equals("sweets")) {
                     price = 1.45;
-                } else if (product.equals("peanuts")){
+                } else if (product.equals("peanuts")) {
                     price = 1.60;
                 }
                 break;
             case "Plovdiv":
                 if (product.equals("coffee")) {
                     price = 0.40;
-                } else if (product.equals("water")){
+                } else if (product.equals("water")) {
                     price = 0.70;
                 } else if (product.equals("beer")) {
                     price = 1.15;
                 } else if (product.equals("sweets")) {
                     price = 1.30;
-                } else if (product.equals("peanuts")){
+                } else if (product.equals("peanuts")) {
                     price = 1.50;
                 }
                 break;
             case "Varna":
                 if (product.equals("coffee")) {
                     price = 0.45;
-                } else if (product.equals("water")){
+                } else if (product.equals("water")) {
                     price = 0.70;
                 } else if (product.equals("beer")) {
                     price = 1.10;
                 } else if (product.equals("sweets")) {
                     price = 1.35;
-                } else if (product.equals("peanuts")){
+                } else if (product.equals("peanuts")) {
                     price = 1.55;
                 }
                 break;
