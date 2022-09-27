922744f1f4267b2ad74f006152e87253bc4da3c7
Denis Buserski <denis.buserski@gmail.com>
2022-09-27 21:57:02 +0300

    Update and rename Conditional_Statements_Advanced_Exercise/Hotel_Room_07.java to Conditional_Statements_Advanced/Exercise/Hotel_Room_07.java

--- Conditional_Statements_Advanced/Exercise/Hotel_Room_07.java
+++ Conditional_Statements_Advanced/Exercise/Hotel_Room_07.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Hotel_Room _07{
+public class Hotel_Room _07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
