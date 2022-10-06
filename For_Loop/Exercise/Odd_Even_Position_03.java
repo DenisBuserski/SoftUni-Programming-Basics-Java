681655be395aba8d393c12ba482488f73d958993
Denis Buserski <denis.buserski@gmail.com>
2022-10-06 21:50:00 +0300

    Update and rename For_Loop_Exercise/Odd_Even_Position.java to For_Loop/Exercise/Odd_Even_Position_03.java

--- For_Loop/Exercise/Odd_Even_Position_03.java
+++ For_Loop/Exercise/Odd_Even_Position_03.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Odd_Even_Position {
+public class Odd_Even_Position_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
