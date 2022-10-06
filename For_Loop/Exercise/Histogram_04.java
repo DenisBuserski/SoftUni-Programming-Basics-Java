64267b5f78ae5672adfe4ad4bcc1499c46d52383
Denis Buserski <denis.buserski@gmail.com>
2022-10-06 21:49:13 +0300

    Update and rename Histogram_03.java to Histogram_04.java

--- For_Loop/Exercise/Histogram_04.java
+++ For_Loop/Exercise/Histogram_04.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Histogram_03 {
+public class Histogram_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
