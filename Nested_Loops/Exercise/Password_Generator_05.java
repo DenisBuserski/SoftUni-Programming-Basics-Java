bd1553df339cb81d03881ecc5dc910431218f216
Denis Buserski <denis.buserski@gmail.com>
2022-10-11 06:41:02 +0300

    Update and rename Nested_Loops_Exercise/Password_Generator.java to Nested_Loops/Exercise/Password_Generator_05.java

--- Nested_Loops/Exercise/Password_Generator_05.java
+++ Nested_Loops/Exercise/Password_Generator_05.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Password_Generator {
+public class Password_Generator_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
