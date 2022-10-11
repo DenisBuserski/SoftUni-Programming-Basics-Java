2e0647999791294ce4ff5d13208b2d8f61237f8c
Denis Buserski <denis.buserski@gmail.com>
2022-10-11 06:33:57 +0300

    Update and rename Nested_Loops_Lab/Cinema_Tickets.java to Nested_Loops/Lab/Cinema_Tickets_07.java

--- Nested_Loops/Lab/Cinema_Tickets_07.java
+++ Nested_Loops/Lab/Cinema_Tickets_07.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Cinema_Tickets {
+public class Cinema_Tickets_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
