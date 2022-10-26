c9cc637aaa4456ba86bfce3a5638fc83637e1372
Denis Buserski <denis.buserski@gmail.com>
2022-10-26 12:47:34 +0300

    Update and rename Programming_Basics_Online_Exam_28_And_29_March_2020/TournamentOfChristmas.java to Exams/Programming_Basics_Online_Exam_28_And_29_March_2020/Tournament_Of_Christmas_06.java

--- Exams/Programming_Basics_Online_Exam_28_And_29_March_2020/Tournament_Of_Christmas_06.java
+++ Exams/Programming_Basics_Online_Exam_28_And_29_March_2020/Tournament_Of_Christmas_06.java
@@ -1,8 +1,6 @@
-package ProgrammingBasicsOnlineExam28And29March2020;
-
 import java.util.Scanner;
 
-public class TournamentOfChristmas {
+public class Tournament_Of_Christmas_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
