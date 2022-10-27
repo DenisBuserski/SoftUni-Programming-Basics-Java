83224364f4c33951a57e45d84e1a5bcfda0050c1
Denis Buserski <denis.buserski@gmail.com>
2022-10-27 09:58:23 +0300

    Update and rename Java_Basics_Exam_21_Sept_2014_Morning/Magic_Sum_02.java to Exams/Java_Basics_Exam_21_Sept_2014_Morning/Magic_Sum_02.java

--- Exams/Java_Basics_Exam_21_Sept_2014_Morning/Magic_Sum_02.java
+++ Exams/Java_Basics_Exam_21_Sept_2014_Morning/Magic_Sum_02.java
@@ -1,5 +1,3 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Archive.Advanced_Java.Advanced_Java_Exams.Java_Basics_Exam_21_Sept_2014_Morning;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
