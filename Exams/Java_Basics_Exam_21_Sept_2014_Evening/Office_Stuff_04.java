7f9298934aa6f7418b71c60ef50bf98e22e0891c
Denis Buserski <denis.buserski@gmail.com>
2022-10-28 12:49:43 +0300

    Update and rename Java_Basics_Exam_21_Sept_2014_Evening/Office_Stuff_04.java to Exams/Java_Basics_Exam_21_Sept_2014_Evening/Office_Stuff_04.java

--- Exams/Java_Basics_Exam_21_Sept_2014_Evening/Office_Stuff_04.java
+++ Exams/Java_Basics_Exam_21_Sept_2014_Evening/Office_Stuff_04.java
@@ -1,5 +1,3 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Archive.Advanced_Java.Advanced_Java_Exams.Java_Basics_Exam_21_Sept_2014_Evening;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -16,14 +14,10 @@ public class Office_Stuff_04 {
             int amount = Integer.parseInt(input[4]);
             String material = input[7];
 
-
-
-
             if (!info.containsKey(company)) {
                 info.put(company, new LinkedHashMap<>());
                 info.get(company).put(material, amount);
             } else {
-
                 if (!info.get(company).containsKey(material)) {
                     info.get(company).put(material, amount);
                 } else {
@@ -33,27 +27,18 @@ public class Office_Stuff_04 {
             }
         }
 
-
         for (Map.Entry<String, Map<String, Integer>> entry : info.entrySet()) {
             System.out.print(entry.getKey() + ": ");
 
             List<String> print;
             List<Map.Entry<String, Integer>> toSort = new ArrayList<>();
 
-            toSort.addAll(entry
-                    .getValue()
-                    .entrySet());
-
+            toSort.addAll(entry.getValue().entrySet());
 
             print = toSort.stream().map(e -> String.format("%s-%d", e.getKey(), e.getValue())).collect(Collectors.toList());
-
             System.out.print(print.toString().replaceAll("[\\[\\]]", ""));
             print.clear();
-
             System.out.println();
-
-
-
         }
     }
 }
