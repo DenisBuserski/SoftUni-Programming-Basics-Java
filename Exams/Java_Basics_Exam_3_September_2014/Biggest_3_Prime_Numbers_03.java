8e93385c78e956a319bc44a87ac35ddeacbf3221
Denis Buserski <denis.buserski@gmail.com>
2022-10-30 11:58:31 +0200

    Update and rename Java_Basics_Exam_3_September_2014/Biggest_3_Prime_Numbers_03.java to Exams/Java_Basics_Exam_3_September_2014/Biggest_3_Prime_Numbers_03.java

--- Exams/Java_Basics_Exam_3_September_2014/Biggest_3_Prime_Numbers_03.java
+++ Exams/Java_Basics_Exam_3_September_2014/Biggest_3_Prime_Numbers_03.java
@@ -1,17 +1,13 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Archive.Advanced_Java.Advanced_Java_Exams.Java_Basics_Exam_3_September_2014;
-
 import java.util.*;
 
 public class Biggest_3_Prime_Numbers_03 {
     public static void main(String[] args) {
         Scanner str = new Scanner(System.in);
         String input = str.nextLine();
+        
+        String[] stringArray = input.split("[( )]+"); // Split the input into string array
 
-        //split the input into string array
-        String[] stringArray = input.split("[( )]+");
-
-        //parse and add numbers to TreeSet for automatic sorting in reverse order
-        TreeSet<Integer> numberSet = new TreeSet<>(Collections.reverseOrder());
+        TreeSet<Integer> numberSet = new TreeSet<>(Collections.reverseOrder()); // Parse and add numbers to TreeSet for automatic sorting in reverse order
         for (int i = 0; i < stringArray.length; i++) {
             if (!stringArray[i].equals("")) {
                 int number = Integer.parseInt(stringArray[i]);
@@ -19,17 +15,16 @@ public class Biggest_3_Prime_Numbers_03 {
             }
         }
 
-        //iterate the numberSet to find the 3 biggest prime numbers
+        // Iterate the numberSet to find the 3 biggest prime numbers
         int primesCount = 0;
         int primesSum = 0;
         for (Integer possiblePrime : numberSet) {
-
-            //exclude the numbers below 1 or all numbers after reaching sum of 3 prime numbers
+            // Exclude the numbers below 1 or all numbers after reaching sum of 3 prime numbers
             if (possiblePrime <= 1 || primesCount == 3) {
                 break;
             }
 
-            //check if the current number is prime number
+            // Check if the current number is prime number
             boolean isPrime = true;
             for (int i = 2; i < possiblePrime; i++) {
                 if (possiblePrime % i == 0) {
@@ -37,14 +32,14 @@ public class Biggest_3_Prime_Numbers_03 {
                 }
             }
 
-            //add the prime number to the sum and incrementing the primesCount
+            // Add the prime number to the sum and incrementing the primesCount
             if (isPrime) {
                 primesSum += possiblePrime;
                 primesCount++;
             }
         }
 
-        //print the result
+        // Print the result
         if (primesCount == 3) {
             System.out.println(primesSum);
         } else {
