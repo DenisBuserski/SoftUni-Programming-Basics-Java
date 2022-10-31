d49531052b64b827910384e32eda623de32cdf7f
Denis Buserski <denis.buserski@gmail.com>
2022-10-31 06:28:55 +0200

    Update and rename CircleAreaAndPerimeter.java to Circle_Area_And_Perimeter.java

--- Simple_Operations_And_Calculations_More_Exercises/Circle_Area_And_Perimeter.java
+++ Simple_Operations_And_Calculations_More_Exercises/Circle_Area_And_Perimeter.java
@@ -1,8 +1,6 @@
-package SimpleOperationsAndCalculationsMoreExercises;
-
 import java.util.Scanner;
 
-public class CircleAreaAndPerimeter {
+public class Circle_Area_And_Perimeter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
