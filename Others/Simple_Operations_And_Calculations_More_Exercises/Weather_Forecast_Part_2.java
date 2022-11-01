db45ca10a0abf50a47773af725311cdaef2ce419
Denis Buserski <denis.buserski@gmail.com>
2022-11-01 07:31:43 +0200

    Update and rename Simple_Operations_And_Calculations_More_Exercises/WeatherForecastPart2.java to Others/Simple_Operations_And_Calculations_More_Exercises/Weather_Forecast_Part_2.java

--- Others/Simple_Operations_And_Calculations_More_Exercises/Weather_Forecast_Part_2.java
+++ Others/Simple_Operations_And_Calculations_More_Exercises/Weather_Forecast_Part_2.java
@@ -1,8 +1,6 @@
-package SimpleOperationsAndCalculationsMoreExercises;
-
 import java.util.Scanner;
 
-public class WeatherForecastPart2 {
+public class Weather_Forecast_Part_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -10,23 +8,17 @@ public class WeatherForecastPart2 {
 
         if (a <= 5.00) {
             System.out.println("unknown");
-        }
-        else if (a <= 11.9){
+        } else if (a <= 11.9) {
             System.out.println("Cold");
-        }
-        else if (a <= 14.9){
+        } else if (a <= 14.9) {
             System.out.println("Cool");
-        }
-        else if (a <= 20.00){
+        } else if (a <= 20.00) {
             System.out.println("Mild");
-        }
-        else if (a <= 25.9){
+        } else if (a <= 25.9) {
             System.out.println("Warm");
-        }
-        else if (a <= 35.00){
+        } else if (a <= 35.00) {
             System.out.println("Hot");
-        }
-        else if (a > 35.00){
+        } else if (a > 35.00) {
             System.out.println("unknown");
         }
     }
