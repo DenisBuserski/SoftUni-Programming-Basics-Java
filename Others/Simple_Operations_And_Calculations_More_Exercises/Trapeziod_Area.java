a5e6be67088d24e3fce68bd5534ea9a89aa9a711
Denis Buserski <denis.buserski@gmail.com>
2022-11-01 07:29:07 +0200

    Update and rename Simple_Operations_And_Calculations_More_Exercises/TrapeziodArea.java to Others/Simple_Operations_And_Calculations_More_Exercises/Trapeziod_Area.java

--- Others/Simple_Operations_And_Calculations_More_Exercises/Trapeziod_Area.java
+++ Others/Simple_Operations_And_Calculations_More_Exercises/Trapeziod_Area.java
@@ -1,8 +1,6 @@
-package SimpleOperationsAndCalculationsMoreExercises;
-
 import java.util.Scanner;
 
-public class TrapeziodArea {
+public class Trapeziod_Area {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
