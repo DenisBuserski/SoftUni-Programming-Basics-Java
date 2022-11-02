4fb2da75b46ce8a91938501f6e9dab3ae7d0e4db
Denis Buserski <denis.buserski@gmail.com>
2022-11-02 08:06:18 +0200

    Update and rename Main.java to Hair_Salon_05.java

--- Exams/Exam_09_2019/Hair_Salon_05.java
+++ Exams/Exam_09_2019/Hair_Salon_05.java
@@ -1,10 +1,6 @@
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Hair_Salon_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
